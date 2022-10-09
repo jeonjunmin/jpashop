989b623fb5859c00a908deb0baf0e08e77980df0
jeonjunmin <azeros314@naver.com>
2022-10-09 17:55:45 +0900

    엔티티 설계시 주의점
    연관관계 메서드:양쪽 Entity를 셋팅할때 한코드로 처리하는 함수

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -36,4 +36,22 @@ public class Order {
     private LocalDateTime orderDate;
 
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //**연관관계 메서드**// --양방향일때 사용하는 메서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
