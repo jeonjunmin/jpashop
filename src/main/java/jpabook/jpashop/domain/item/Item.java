8e0d5b704c32a5267b4226e4b48c282a88888741
jeonjunmin <azeros314@naver.com>
2022-10-13 22:58:15 +0900

    fix: item 엔티티 비지니스 로직 오타 수정
    removeStock 오타 수정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -38,7 +38,7 @@ public abstract class Item {
 
     //스톡 감소
     public void removeStock(int quantity) {
-        int restStock = this.stockQuantity = quantity;
+        int restStock = this.stockQuantity - quantity;
         if(restStock < 0){
             throw new NotEnoughStockException("need more stock");
         }
