c07f84fe6ee2853288a6b6511b37a2e7d3a79bd7
jeonjunmin <azeros314@naver.com>
2022-10-21 19:28:05 +0900

    회원 등록
    회원 등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute ("memberForm", new MemberForm());  //memberForm을 model에 담아 createMemberForm 화면으로 넘긴다.
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result){ //@Valid는 MemberForm의 @NotEmpty같은 유효성 검사를 사용하겠다는 표시이다.
+
+        //BindingResult를 사용하여 에러시 화면 컨트롤을 할 수 있다.
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/"; //액션 동작 후 home 페이지로 보낸다.
+    }
+}
---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotEmpty;
+
+
+@Getter @Setter
+@Slf4j
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
