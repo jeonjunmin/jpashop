025de1711a627e81626aabf91e50ea41601986a8
jeonjunmin <azeros314@naver.com>
2022-10-23 22:40:55 +0900

    변경 감지와 병합
    변경 감지와 병합

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,21 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+
+    /**
+     * 변경 감지에 의한 데이터 업데이트
+     * merge를 사용하면 지정되지 않은 필드에 null 값이 들어갈 위험이 있어 필드 하나하나를 지정할 수 있는 변경감지 업데이트를 사용하는 것이 안전하다.
+     */
+    @Transactional //@Transactional을 붙여주면 아래 Entity에 값을 set해준것 만으로 DB에 커밋이 일어난다.
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+
+        return findItem;
     }
 
     public List<Item> findItems() {
